230ecf78136d968a21bf1fbf8e486641398e950a
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-04 20:58:12 -0500

    Setup Karma testing. Rollup bundles each test and converts it to  ES5 before serving to the browser.

---
+++ src/app/app.spec.js
@@ -0,0 +1,22 @@
+import 'angular';
+import 'angular-mocks';
+import app, { AppService } from './app';
+
+beforeEach(module('app'));
+describe('app ->', function () {
+    describe('AppService:', function () {
+        it('method should resolve something.', inject(function ($injector) {
+            let $rootScope = $injector.get('$rootScope');
+            let $q = $injector.get('$q');
+
+            let service = new AppService($q);
+
+            service.resolveValue()
+                .then(function (value) {
+                    expect(value).toEqual(42);
+                });
+
+            $rootScope.$digest();
+        }));
+    });
+});

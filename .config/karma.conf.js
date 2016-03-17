de713504aee6a247ef01b7aa0ef7c35c00a34c51
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-16 22:12:17 -0400

    Add comments to Karma Rollup preprocessing.

--- .config/karma.conf.js
+++ .config/karma.conf.js
@@ -32,15 +32,18 @@ module.exports = function(config) {
         rollup: {
             external: ['angular'], // Load Angular + Mocks via Karma instead to avoid angular-mocks multi-import bug.
             plugins: [
+                // Transform ES2015 to ES5, sans module imports/exports
                 babel({
                     include: 'src/**',
                     exclude: 'node_modules/**'
                 }),
+                // Resolve any non-relative module imports using NPM
                 node_resolve({
                     jsnext: true,
                     main: true,
                     browser: true
                 }),
+                // Transform CommonJS modules from NPM into ES2015 modules, suitable for bundling via rollup
                 commonjs({
                     include: 'node_modules/**',
                     exclude: 'src/**'
@@ -48,8 +51,8 @@ module.exports = function(config) {
             ] 
         },
         bundle: {
-            format: 'iife',
-            sourceMap: 'inline'
+            format: 'iife', // Transpiled ES5 exported as a global module.
+            sourceMap: 'inline' // For use by Karma in stack traces and code coverage
         }
     }
   })

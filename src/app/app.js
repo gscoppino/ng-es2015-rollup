0e8fbfe1609e27f8c3d81051f60c5a68d1303aef
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-21 23:59:07 -0400

    Fix strict di mistake.

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,13 @@
 import angular from 'angular';
 import Routes from './routes/routes';
 
+Run.$inject = ['$log'];
+function Run ($log) {
+    $log.log('Loaded!');
+}
+
 const AppModule = angular
     .module('app', [Routes])
-        .run(($log)=> {
-            $log.log('Loaded!');
-        });
+        .run(Run);
 
 export default AppModule.name;

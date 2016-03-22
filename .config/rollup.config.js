5b027ddd2155a148b740d8345ef2f4acc120dfca
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-21 23:04:05 -0400

    Duplicate comments from Karam rollup pipeline into main rollup config.

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -1,20 +1,23 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import node_resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     rollup: {
         entry: 'src/main.js',
         plugins: [
+            // Transform ES2015 to ES5, sans module imports/exports
             babel({
                 include: 'src/**',
                 exclude: 'node_modules/**'
             }),
+            // Resolve any non-relative module imports using NPM
             node_resolve({
                 jsnext: true,
                 main: true,
                 browser: true
             }),
+            // Transform CommonJS modules from NPM into ES2015 modules, suitable for bundling via rollup
             commonjs({
                 include: 'node_modules/**',
                 exclude: 'src/**'

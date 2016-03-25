7b4902e223d503b77a5007749e8e3a3f89b9e239
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-25 13:02:04 -0400

    Remove unused imports from karma.config.js.

--- gulp/test/karma.config.js
+++ gulp/test/karma.config.js
@@ -1,7 +1,3 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import node_resolve from 'rollup-plugin-node-resolve';
-
 import ROLLUP_CONFIG from './rollup.config';
 
 export default {

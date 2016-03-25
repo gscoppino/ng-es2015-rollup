45756bcefd6a6b0407efeef316c8e56a179077fe
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-25 15:19:31 -0400

    Add HTML loader so Angular templates can be in their own files. Add sample component for testing sake.

---
+++ src/app/components/SampleComponent/SampleComponent.spec.js
@@ -0,0 +1,22 @@
+import SampleComponentModule from './SampleComponent';
+
+beforeEach(module(SampleComponentModule));
+
+describe('SampleComponent', () => {
+    describe('Template', () => {
+        let $rootScope, $compile;
+
+        beforeEach(inject(($injector) => {
+            $rootScope = $injector.get('$rootScope');
+            $compile = $injector.get('$compile');
+        }));
+
+        it('should compile.', () => {
+            let scope = $rootScope.$new(),
+                element = $compile('<sample-component></sample-component>')(scope);
+
+            scope.$digest();
+            expect(element.html()).toEqual('Sample Text');
+        });
+    });
+});

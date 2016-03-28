09a0e95b7380da7bda3296bd1d089a07c1bad0b6
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-03-28 17:19:35 -0400

    Add support for routing via HTML5 history api to the app and the devserver.

--- gulp/serve/tasks.js
+++ gulp/serve/tasks.js
@@ -1,14 +1,16 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
+import historyApiMiddleware from 'connect-history-api-fallback';
 
 gulp.task('devserver', ()=> {
     let server = browserSync.create();
-    
+
     server.init({
         server: {
-            baseDir: 'dist'
+            baseDir: 'dist',
+            middleware: historyApiMiddleware({ index: '/index.html' })
         }
     });
-    
+
     gulp.watch('dist/**/*', server.reload);
 });
--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,5 +1,18 @@
 import angular from 'angular';
 
-const RouteModule = angular.module('app.routes', []);
-    
-export default RouteModule.name;
+RouterConfig.$inject = ['$locationProvider'];
+function RouterConfig($locationProvider) {
+    /*
+     * Attempt to use HTML5 History API for routing, if available.
+     * Otherwise, route using hashbangs.
+     * NOTE: HTML5 mode requires proper server side configuration.
+     * NOTE: HTML5 mode requires the existence of a <base> tag in HTML.
+     */
+    $locationProvider.html5Mode(true);
+}
+
+const RouterModule = angular
+    .module('app.routes', [])
+    .config(RouterConfig);
+
+export default RouterModule.name;

b37af135918b53d1cc300f4e3e4f7cecbbbee6d9
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-05-11 02:10:12 -0400

    Add CSS linting and correct some minor CSS oversights found using the tool.

--- gulp/build/css/postcss.config.js
+++ gulp/build/css/postcss.config.js
@@ -1,15 +1,19 @@
+import stylelint from 'stylelint';
+import reporter from 'postcss-reporter';
 import cssimport from 'postcss-import';
 import cssnext from 'postcss-cssnext';
 
 export default {
-    from: 'src/main.css', 
+    from: 'src/main.css',
     to: 'dist/main.css',
     map: { inline: false },
     plugins: [
         cssimport(),
+        stylelint(),
+        reporter(),
         cssnext({
             browsers: [
-                "IE 11", 
+                "IE 11",
                 "last 2 Chrome versions",
                 "last 2 Firefox versions",
                 "last 2 Edge versions",
@@ -17,7 +21,7 @@ export default {
                 "last 2 Android versions",
                 "last 2 ChromeAndroid versions",
                 "last 2 iOS versions",
-                "last 2 ExplorerMobile versions"               
+                "last 2 ExplorerMobile versions"
             ]
         })
     ]

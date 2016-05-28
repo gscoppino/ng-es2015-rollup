68c38834b79a628d0dce336b6d4b33a97918672a
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-05-28 15:58:47 -0400

    Create directives folder, add sample directive which demonstrates module resolution from app root.

---
+++ src/app/directives/sample-directive/index.spec.js
@@ -0,0 +1,30 @@
+import angular from 'angular';
+import SampleDirectiveModule from './';
+
+beforeEach(module(SampleDirectiveModule));
+
+describe('SampleDirective', ()=> {
+    let $rootScope, $compile, SampleService, element;
+
+    beforeEach(inject(($injector) => {
+        $rootScope = $injector.get('$rootScope');
+        $compile = $injector.get('$compile');
+        SampleService = $injector.get('SampleService');
+
+        spyOn(SampleService, 'log').and.callFake(angular.noop);
+
+        element = $compile('<div sample-directive></div>')($rootScope.$new());
+        $rootScope.$digest();
+    }));
+
+    it('should log something.', () => {
+        expect(SampleService.log).toHaveBeenCalled();
+    });
+
+    it('should have prepended to the element.', () => {
+        let firstChildEl = element.children()[0];
+
+        expect(firstChildEl.nodeName).toEqual('DIV');
+        expect(firstChildEl.innerHTML).toEqual('Sample Text');
+    });
+});

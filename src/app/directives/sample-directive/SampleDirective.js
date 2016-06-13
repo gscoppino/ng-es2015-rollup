5e6930fc3fd727ce0acb7ec3dce9b7fae5f5f279
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-06-13 14:08:31 -0400

    Move sample directive definition out of inline declaration.

--- src/app/directives/sample-directive/SampleDirective.js
+++ src/app/directives/sample-directive/SampleDirective.js
@@ -1,15 +1,14 @@
 import angular from 'angular';
 import SampleService from 'app/services/SampleService/SampleService';
 
-const SampleDirective = angular.module('app.directives.sample', [SampleService])
-    .directive('sampleDirective', (SampleService)=> {
-        return {
-            restrict: 'A',
-            link: function (scope, element) {
-                SampleService.log();
-                element.prepend('<div>Sample Text</div>');
-            }
-        };
-    });
+const SampleDirective = (SampleService) => ({
+    restrict: 'A',
+    link: function (scope, element) {
+        SampleService.log();
+        element.prepend('<div>Sample Text</div>');
+    }
+});
 
-export default SampleDirective.name;
+export default angular.module('app.directives.sample', [SampleService])
+    .directive('sampleDirective', SampleDirective)
+    .name;

429cac4901663d30c9937780bbc1c3eb2e818035
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-06-28 13:50:19 -0400

    Don't use global module and inject in tests.

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,7 +1,8 @@
+import angular from 'angular';
 import App from './app';
 
-beforeEach(module(App));
+beforeEach(angular.mock.module(App));
 
-it('should log to the console.', inject(($log) => {
+it('should log to the console.', angular.mock.inject(($log) => {
     expect($log.log.logs.length).toEqual(1);
 }));

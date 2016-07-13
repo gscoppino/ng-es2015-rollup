c914ffc9ebdb1d8e0efd5aebcfdf35a6df500132
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-07-13 04:21:49 -0400

    Use root app component like Angular 2.

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,21 @@
 import angular from 'angular';
+import template from './app.html';
 import Api from './api/api';
 import Routes from './routes/routes';
 
-Run.$inject = ['$log'];
-function Run ($log) {
-    $log.log('Loaded!');
+class AppController {
+    static $inject = ['$log'];
+    static bindings = {};
+
+    constructor($log) {
+        $log.log('Loaded!');
+    }
 }
 
 export default angular.module('app', [Api, Routes])
-    .run(Run)
+    .component('app', {
+        template,
+        controller: AppController,
+        bindings: AppController.bindings
+    })
     .name;
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -3,6 +3,40 @@ import App from './app';
 
 beforeEach(angular.mock.module(App));
 
-it('should log to the console.', angular.mock.inject(($log) => {
-    expect($log.log.logs.length).toEqual(1);
-}));
+describe('App Component', () => {
+    describe('View', () => {
+        let $rootScope, $compile;
+        beforeEach(angular.mock.inject(($injector) => {
+            $rootScope = $injector.get('$rootScope');
+            $compile = $injector.get('$compile');
+        }));
+
+        it('should compile', () => {
+            let scope = $rootScope.$new(),
+                element = $compile('<app></app>')(scope);
+
+            scope.$digest();
+            expect(element.html().trim()).toEqual('Hello World');
+        });
+    });
+
+    describe('Controller', () => {
+        let $log, $componentController;
+        beforeEach(angular.mock.inject(($injector) => {
+            $log = $injector.get('$log');
+            $componentController = $injector.get('$componentController');
+        }));
+
+        it('should exist.', () => {
+            let controller = $componentController('app');
+            expect(controller).toBeDefined();
+        });
+
+        it('should log to the logger.', () => {
+            spyOn($log, 'log').and.callFake(angular.noop);
+            $componentController('app');
+
+            expect($log.log).toHaveBeenCalledWith('Loaded!');
+        });
+    });
+});

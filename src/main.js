722fa53a8bf437fe5dfd6132982f0213ea56c535
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-07-23 15:02:06 -0400

    Check if DOMContentLoaded was already fired in bootstrap (allows compatibility with SystemJS).

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import angular from 'angular';
 import App from './app/app';
 
-document.addEventListener('DOMContentLoaded', () => {
+if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    bootstrap();
+} else {
+    document.addEventListener('DOMContentLoaded', bootstrap);
+}
+
+function bootstrap() {
     angular.bootstrap(document.documentElement, [App], { strictDi: true });
-});
+}

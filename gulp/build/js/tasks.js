aba9447f1bebf8e70a0130e42f0d10b7daca76a7
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-07-27 22:51:35 -0400

    Cache rollup bundles for incremental builds.

--- gulp/build/js/tasks.js
+++ gulp/build/js/tasks.js
@@ -4,17 +4,27 @@ import { rollup } from 'rollup';
 import ROLLUP_DEV_CONFIG from './rollup.dev';
 import ROLLUP_PRODUCTION_CONFIG from './rollup.production';
 
+let cache; // used for watched builds
+
 gulp.task('clean:js', ()=> del([ROLLUP_DEV_CONFIG.bundle.dest, `${ROLLUP_DEV_CONFIG.bundle.dest}.map`, ROLLUP_PRODUCTION_CONFIG.bundle.dest]));
 
 gulp.task('build:js', ['clean:js'], ()=> {
-    return rollup(ROLLUP_DEV_CONFIG.rollup)
-        .then((bundle)=> bundle.write(ROLLUP_DEV_CONFIG.bundle));
+    return rollup(Object.assign({}, ROLLUP_DEV_CONFIG.rollup, { cache }))
+        .then((bundle)=> {
+            cache = bundle;
+            console.log('cached bundle for later use.');
+            return bundle.write(ROLLUP_DEV_CONFIG.bundle);
+        });
 });
 
 gulp.task('watch:js', ['build:js'], ()=> gulp.watch(['src/**/!(*.spec).js', 'src/app/**/*.html'], ['build:js']));
 
 
 gulp.task('build:js-production', ['clean:js'], ()=> {
-    return rollup(ROLLUP_PRODUCTION_CONFIG.rollup)
-        .then((bundle)=> bundle.write(ROLLUP_PRODUCTION_CONFIG.bundle));
+    return rollup(Object.assign({}, ROLLUP_PRODUCTION_CONFIG.rollup, { cache }))
+        .then((bundle)=> {
+            cache = bundle;
+            console.log('cached bundle for later use.');
+            return bundle.write(ROLLUP_PRODUCTION_CONFIG.bundle)
+        });
 });

a3a378e8ea93bb25f87b740079aa9329f49248c1
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-08-19 21:22:27 -0400

    Add comments to angular bootstrapping code.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import angular from 'angular';
 import App from './app/app';
 
-if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    bootstrap();
-} else {
-    document.addEventListener('DOMContentLoaded', bootstrap);
-}
-
 function bootstrap() {
     angular.bootstrap(document.documentElement, [App], { strictDi: true });
+}
+
+if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    // If the document readyState is "interactive", then the DOMContentLoaded event
+    // has already been fired. If the readyState is "complete", then the readyState
+    // has already transitioned from "interactive" at some point in the past.
+    // Either way, we should perform the bootstrap.
+    bootstrap();
+} else {
+    // Wait for the document to be ready before performing the bootstrap.
+    document.addEventListener('DOMContentLoaded', bootstrap);
 }

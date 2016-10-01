454cbae12bc3889f40d75f40f10ca5df9eeeaeac
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-10-01 13:15:28 -0400

    Modified gulp folder structure and file formatting a bit.

--- gulp/main.js
+++ gulp/main.js
@@ -1,27 +1,48 @@
 import gulp from 'gulp';
 
 import './git/tasks';
-import './build/html/tasks';
-import './build/css/tasks';
-import './build/js/tasks';
-import './build/images/tasks';
-import './build/worker/tasks';
-import './build/docs/tasks';
+import './html/tasks';
+import './css/tasks';
+import './js/tasks';
+import './images/tasks';
+import './worker/tasks';
+import './docs/tasks';
 import './test/tasks';
 import './serve/tasks';
 import './generate/tasks';
 
-gulp.task('clean', ['clean:markup', 'clean:css', 'clean:js', 'clean:images',
-                    'clean:worker', 'clean:docs', 'clean:test-coverage']);
+gulp.task('clean', [
+    'clean:markup',
+    'clean:css',
+    'clean:js',
+    'clean:images',
+    'clean:worker',
+    'clean:docs',
+    'clean:test-coverage'
+]);
 
-gulp.task('build', ['build:markup', 'build:css', 'build:js', 'build:images']);
+gulp.task('build', [
+    'build:markup',
+    'build:css',
+    'build:js',
+    'build:images'
+]);
 
-gulp.task('watch', ['watch:markup', 'watch:css', 'watch:js', 'watch:images',
-                    'watch:git']);
+gulp.task('watch', [
+    'watch:markup',
+    'watch:css',
+    'watch:js',
+    'watch:images',
+    'watch:git'
+]);
+
+gulp.task('build-production', [
+    'build:markup-production',
+    'build:css-production',
+    'build:js-production',
+    'build:images',
+    'build:worker'
+]);
 
 gulp.task('develop', ['watch'], ()=> gulp.start('devserver'));
-
-gulp.task('build-production', ['build:markup-production', 'build:css-production', 'build:js-production', 'build:images',
-          'build:worker']);
-
 gulp.task('production', ['build-production'], ()=> gulp.start('devserver'));

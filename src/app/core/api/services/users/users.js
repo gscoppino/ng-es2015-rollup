ae1ae4e6e14c06aadb890c3e51154a0d8676520a
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-10-27 11:43:18 -0400

    Fix UserService based on changes to master.

--- src/app/core/api/services/users/users.js
+++ src/app/core/api/services/users/users.js
@@ -1,11 +1,11 @@
 import angular from 'angular';
-import Api from 'app/core/api/api';
+import ApiFactory from 'app/core/api/factory/factory';
 
-UserService.$inject = ['Restangular'];
-function UserService(Restangular) {
-    return Restangular.service('users');
+UserService.$inject = ['ApiFactory'];
+function UserService(ApiFactory) {
+    return ApiFactory.create('users');
 }
 
-export default angular.module('app.api.services.users', [Api])
+export default angular.module('app.api.services.users', [ApiFactory])
     .factory('UserService', UserService)
     .name;

312bafc19233173de5cce04489db24abf4422ddd
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-11-10 15:26:00 -0500

    Add comments in gulp/rollup.config.js explaining how Gulp system works in this project.

--- tasks/rollup.config.js
+++ tasks/rollup.config.js
@@ -1,3 +1,11 @@
+// This config file is used to create a Gulpfile in ES5 from the
+// ES2015 source code in this folder. This normally will happen
+// automatically after an `npm install` is completed. If a change
+// to the source code in this folder is made after installing dependencies,
+// then the generated gulpfile will need to be manually rebuilt. See `package.json`
+// to reference the correct script to run in order to rebuild the Gulpfile, or just
+// run `npm install` again.
+
 import babel from 'rollup-plugin-babel';
 
 export default {

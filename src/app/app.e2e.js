abbd072414b6b4be669786627f2216597a2ff245
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-11-24 22:48:45 -0500

    Add integration testing with Protractor.

---
+++ src/app/app.e2e.js
@@ -0,0 +1,13 @@
+describe('App Start', () => {
+    it('should render the app component.', () => {
+        browser.get('/');
+
+        let appElement = element(by.tagName('app'));
+
+        expect(appElement.isElementPresent(by.tagName('loader-spinner')))
+            .toBe(true);
+
+        expect(appElement.isElementPresent(by.css('main[ui-view]')))
+            .toBe(true);
+    });
+});

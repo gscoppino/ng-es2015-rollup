830207f61cb28d815be8ec01987214dcb56c244e
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2017-05-12 00:34:33 -0400

    (App) Update JSDoc.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,5 +1,6 @@
 /**
  * Controller for the application root component.
+ * @memberof app
  */
 class AppController {
 
@@ -59,8 +60,8 @@ class AppController {
  * @memberof app
  * @name AppComponent
  * @desc The root component of the application.
- * @property controller {function} - [AppController]{@link AppController}
- * @property bindings {object} - [AppController.bindings]{@link AppController.bindings}
+ * @property controller {function} - [AppController]{@link app.AppController}
+ * @property bindings {object} - [AppController.bindings]{@link app.AppController.bindings}
  */
 export default {
     controller: AppController,

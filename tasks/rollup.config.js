7fd00effd127dead96d01eba02fff5b9e1fabab2
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2017-09-05 18:11:05 -0400

    Remove a bad instruction in build system documentation.

--- tasks/rollup.config.js
+++ tasks/rollup.config.js
@@ -3,8 +3,7 @@
 // automatically after an `npm install` is completed. If a change
 // to the source code in this folder is made after installing dependencies,
 // then the generated gulpfile will need to be manually rebuilt. See `package.json`
-// to reference the correct script to run in order to rebuild the Gulpfile, or just
-// run `npm install` again.
+// to reference the correct script to run in order to rebuild the Gulpfile.
 
 import babel from 'rollup-plugin-babel';
 

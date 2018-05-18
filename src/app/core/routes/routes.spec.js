ed9419d00a01d46e8f95d63a647d11cb71c738d3
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-05-18 02:51:49 -0400

    (tests) Simplify/improve test for HTML5 mode enabled.

--- src/app/core/routes/routes.spec.js
+++ src/app/core/routes/routes.spec.js
@@ -2,19 +2,16 @@ import angular from 'angular';
 
 import RoutesModule from './routes.module.js';
 
-beforeEach(angular.mock.module(RoutesModule));
+let $locationProvider;
+
+beforeEach(angular.mock.module(RoutesModule, (_$locationProvider_) => {
+    $locationProvider = _$locationProvider_;
+}));
 
 describe('Routes Configuration', () => {
-    it('should be in HTML5 mode.', angular.mock.inject(($location) => {
-        try {
-            // This function throws an exception if used as a setter, when not in HTML5 mode.
-            // This is not the best way to do the test since it relies on knowledge of how this particular
-            // function in the Angular API is written (the fact that it will throw an exception is not documented).
-            $location.state({});
-        } catch (exception) {
-            fail();
-        }
-    }));
+    it('should be in HTML5 mode.', () => {
+        expect($locationProvider.html5Mode().enabled).toBe(true);
+    });
 
     it('should have loaded the default route.', angular.mock.inject(($rootScope, $state) => {
         // This test could be better but is OK for now.

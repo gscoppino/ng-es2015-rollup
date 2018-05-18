27e3b0c8c0ce34fc6cfbf9e80e10ac5d62e9d277
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-05-18 01:34:35 -0400

    (app) Use non-deprecated method of setting up unknown route redirection.

--- src/app/core/routes/routes.module.js
+++ src/app/core/routes/routes.module.js
@@ -1,13 +1,13 @@
 import angular from 'angular';
 import UIRouterModule from '@uirouter/angularjs';
 
-RouterConfig.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+RouterConfig.$inject = ['$locationProvider', '$stateProvider', '$urlServiceProvider'];
 /**
  * @memberof module:RoutesModule
  * @description Attempt to use HTML5 History API for routing, if available.
  *   Otherwise, route using hashbangs.
  */
-function RouterConfig($locationProvider, $stateProvider, $urlRouterProvider) {
+function RouterConfig($locationProvider, $stateProvider, $urlServiceProvider) {
     // NOTE: HTML5 mode requires proper server side configuration.
     // NOTE: HTML5 mode requires the existence of a <base> tag in HTML.
     $locationProvider.html5Mode(true);
@@ -17,7 +17,7 @@ function RouterConfig($locationProvider, $stateProvider, $urlRouterProvider) {
         template: '<h1>App</h1>'
     });
 
-    $urlRouterProvider.otherwise('/');
+    $urlServiceProvider.rules.otherwise({ state: 'app' });
 }
 
 export { RouterConfig };
